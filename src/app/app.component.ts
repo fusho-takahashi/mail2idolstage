184ccbddb6bf41199d1f97d02fbbb24020c58cfb
fusho-takahashi <zenryaku@fusho-takahashi.me>
2021-02-13 22:29:15 +0900

    implements form & add styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DateAdapter, NativeDateAdapter } from '@angular/material/core';
+
+interface FormItems {
+  date: Date;
+  section: number;
+  nickname: string;
+  tel: string;
+  email: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,19 +16,58 @@ import { DateAdapter, NativeDateAdapter } from '@angular/material/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(private dateAdapter: DateAdapter<NativeDateAdapter>) {
+  constructor(
+    private dateAdapter: DateAdapter<NativeDateAdapter>,
+    private fb: FormBuilder,
+  ) {
     dateAdapter.setLocale('ja');
   }
 
   readonly IDOL_STAGE_ADDRESS = 'idolstage.reservation@gmail.com';
+  readonly minDate = new Date();
 
-  section: 1 | 2 | 3 | null = null;
-  nickname = '';
-  tel = '';
-  mail = '';
+  form: FormGroup = this.fb.group({
+    date: [null, [Validators.required]],
+    section: [null, [Validators.required]],
+    nickname: [null, [Validators.required]],
+    tel: [null, [Validators.required]],
+    email: [null, [Validators.required]],
+  });
 
-  get minDate(): Date {
-    const today = new Date();
-    return new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
+  onSubmit(): void {
+    this.form.markAllAsTouched();
+    if (this.form.invalid) {
+      return;
+    }
+
+    const formValue: FormItems = this.form.getRawValue();
+
+    const subject = this.createSubject(formValue.date, formValue.section);
+    const body = this.createBody(
+      formValue.nickname,
+      formValue.tel,
+      formValue.email,
+    );
+
+    window.location.href = `mailto:${this.IDOL_STAGE_ADDRESS}?subject=${subject}&body=${body}`;
+  }
+
+  createSubject(inputDate: Date, section: number): string {
+    const month = inputDate.getMonth() + 1;
+    const date = inputDate.getDate();
+    const day = inputDate.getDay();
+
+    return `【${month}/${date}(${this.convertDayToJp(
+      day,
+    )}) ${section}部 観覧希望】`;
+  }
+
+  convertDayToJp(day: number): string {
+    const dayOfWeekJp = ['日', '月', '火', '水', '木', '金', '土'];
+    return dayOfWeekJp[day];
+  }
+
+  createBody(nickname: string, tel: string, email: string): string {
+    return `・名前: ${nickname}%0D%0A・電話番号: ${tel}%0D%0A・メールアドレス: ${email}%0D%0A %0D%0Aよろしくお願いします。`;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FormsModule,
     MatDatepickerModule,
     MatFormFieldModule,
     MatNativeDateModule,
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
